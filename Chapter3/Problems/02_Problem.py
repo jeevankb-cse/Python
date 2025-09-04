79a201525bffd2db88061983ba2c5176d7e4466e
Jeevan K B <jeevankenchalli514@gmail.com>
2025-09-04 19:35:58 +0530

    02_Problem.py

--- Chapter3/Problems/02_Problem.py
+++ Chapter3/Problems/02_Problem.py
@@ -1,5 +1,12 @@
+#Q.Write a program to fill in a letter template given below with name and date.
+#letter =
+#Dear </Name>,
+#You are selected! </Date|>
+
+
 letter='''Dear <|Name|>,
 You are selected!
 <|Date|>'''
         
+
 print(letter.replace("<|Name|>","Jeevan").replace("<|Date|>","29/7/2025"))        
