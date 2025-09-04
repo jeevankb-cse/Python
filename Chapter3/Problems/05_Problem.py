232c1859b2f399ff3ab6f00af16b71ebde6be7b2
Jeevan K B <jeevankenchalli514@gmail.com>
2025-09-04 19:38:47 +0530

    05_Problem.py

--- Chapter3/Problems/05_Problem.py
+++ Chapter3/Problems/05_Problem.py
@@ -1,2 +1,8 @@
-letter="Dear Jeevan,\n\tThis python course is nice.\n\tThanks!"
-print(letter)
+#Q.Write a program to format the following letter using escape sequence characters.
+#Letter "Dear Harry, this python course is nice. Thanks!"
+
+
+
+Letter="Dear Jeevan,\n\tThis python course is nice.\n\tThanks!"
+
+print(Letter)
