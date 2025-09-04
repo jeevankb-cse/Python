af4ee53f9704e9946334c9796a810c94f398c036
Jeevan K B <jeevankenchalli514@gmail.com>
2025-09-04 19:37:29 +0530

    04_Problem.py

--- Chapter3/Problems/04_Problem.py
+++ Chapter3/Problems/04_Problem.py
@@ -1,3 +1,6 @@
+#Q.Replace the double space from problem 3 with single spaces.
+
 name="Jeevan is very very bad  boy and  "
 print(name.replace("  "," "))
+
 print(name)#Strings are immutable which means that you cannot change by running functions on them
