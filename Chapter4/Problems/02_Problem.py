535145442593f43824c7f2483f53f6d4ac8e1ee4
Jeevan K B <jeevankenchalli514@gmail.com>
2025-09-04 19:45:16 +0530

    02_Problem.py

--- Chapter4/Problems/02_Problem.py
+++ Chapter4/Problems/02_Problem.py
@@ -1,3 +1,4 @@
+#Q.Write a program to accept marks of 3 students and display them in a sorted manner.
 marks=[]
 f1=int(input("Enter marks here:"))
 marks.append(f1)
@@ -6,4 +7,5 @@ marks.append(f2)
 f3=int(input("Enter marks here:"))
 marks.append(f3)
 marks.sort()
+
 print(marks)
