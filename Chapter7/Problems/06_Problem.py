00cf64e3bc82f38b55c608e33b072a644cc78bb5
Jeevan K B <jeevankenchalli514@gmail.com>
2025-10-03 20:39:34 +0530

    06_Problem.py

--- Chapter7/Problems/06_Problem.py
+++ Chapter7/Problems/06_Problem.py
@@ -1,3 +1,4 @@
+#Write a program to calculate the factorial of a given number using for loop.
 # 5! = 1 X 2 X 3 X 4 X 5
 n=int(input("Enter a number:")) 
 product = 1
@@ -5,5 +6,6 @@ for i in range(1,n+1):
     product=product*i
 print(product)   #(OR)print(f"The factorial of {n} is {product}")
   
+
 
     
