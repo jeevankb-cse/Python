c33bf65fe9f04703b08d7a49eb7eb32772479835
Jeevan K B <jeevankenchalli514@gmail.com>
2025-10-03 20:38:04 +0530

    04_Problem.py

--- Chapter7/Problems/04_Problem.py
+++ Chapter7/Problems/04_Problem.py
@@ -1,3 +1,5 @@
+#Write a program to find whether a given number is prime or not?
+
 n=int(input("Enter a number:"))
 
 for i in range(2,n):
@@ -6,4 +8,5 @@ for i in range(2,n):
         break
     else:
         print("Number is prime")
+
         
